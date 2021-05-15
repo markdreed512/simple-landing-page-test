b773581c3ca999845df5853a0f1966d1b9337242
Mark Reed <markdreed512@gmail.com>
2021-05-15 12:35:06 -0500

    comments

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
 // need to reset modals after submit
 // need to reconfigure contact modal in .html, not here
-$('#modal-form').on("submit", handleSubmit)
-$('#contact-btn').on("click", handleContactClick)
+// fix gap on right
+$('#demo-modal-form').on("submit", handleDemoSubmit)
+$('#contact-modal-form').on("submit", handleContactSubmit)
+// $('#contact-btn').on("click", handleContactClick)
 
 
-function handleContactClick(){
-    $("#modalLabel").html(`<h3>Contact Us</h3>`)
-    let contactHTML = `
-        <div class="mb-3">
-            <label for="firstName" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="firstName" aria-describedby="firstName">
-        </div>
-        <div class="mb-3">
-            <label for="lastName" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="lastName">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email">
-        </div>
-        <div class="mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea type="text" class="form-control" id="email"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    `
-    $(".modal-body").html(contactHTML)
-}
-function throwConfetti(){
+// function handleContactClick(){
+//     $("#demo-modalLabel").html(`<h3>Contact Us</h3>`)
+//     let contactHTML = `
+//         <div class="mb-3">
+//             <label for="firstName" class="form-label">First Name</label>
+//             <input type="text" class="form-control" id="firstName" aria-describedby="firstName">
+//         </div>
+//         <div class="mb-3">
+//             <label for="lastName" class="form-label">Last Name</label>
+//             <input type="text" class="form-control" id="lastName">
+//         </div>
+//         <div class="mb-3">
+//             <label for="email" class="form-label">Email</label>
+//             <input type="email" class="form-control" id="email">
+//         </div>
+//         <div class="mb-3">
+//             <label for="message" class="form-label">Message</label>
+//             <textarea type="text" class="form-control" id="email"></textarea>
+//         </div>
+//         <button type="submit" class="btn btn-primary">Submit</button>
+//     `
+//     $(".modal-body").html(contactHTML)
+// }
+
+function handleDemoSubmit(e){
+    e.preventDefault()
+    let name = e.target.children[0].children[1].value
+    $("#demo-modalLabel").html(`<h3>Success!!</h3>`)
+    $("#demo-modal-body").html(`<h4>Thanks ${name}! You'll be hearing from one of our agents soon.</h4><button type="button" class="btn btn-secondary mt-5" data-bs-dismiss="modal">Close</button>`)
+    throwConfetti("top")
+}  
+function handleContactSubmit(e){
+    e.preventDefault()
+    let name = e.target.children[0].children[1].value
+    $("#contact-modalLabel").html(`<h3>Success!!</h3>`)
+    $("#contact-modal-body").html(`<h4>Thanks ${name}! You'll be hearing from one of our agents soon.</h4><button type="button" class="btn btn-secondary mt-5" data-bs-dismiss="modal">Close</button>`)
+    throwConfetti("middle")
+}  
+function throwConfetti(startingPoint){
     for(let i=0; i < 100; i++){
-        let width = 5 + Math.floor(Math.random() * 10)
-        let height = 10 + Math.floor(Math.random() * 20)
-        let red = Math.floor(Math.random() * 255)
-        let green = Math.floor(Math.random() * 255)
-        let blue = Math.floor(Math.random() * 255)
-        let c = new Confetti(width, height, red, green, blue)
+        let c = new Confetti(startingPoint)
         c.show()
     }
 }
-function handleSubmit(e){
-    e.preventDefault()
-    let name = e.target.children[0].children[1].value
-    $("#modalLabel").html(`<h3>Success!!</h3>`)
-    $(".modal-body").html(`<h4>Thanks ${name}! You'll be hearing from one of our agents soon.</h4><button type="button" class="btn btn-secondary mt-5" data-bs-dismiss="modal">Close</button>`)
-    throwConfetti()
-}  
-
 class Confetti{
-    constructor(w,h,r,g,b){
-        this.w = w
-        this.h = h
-        this.color = `rgb(${r},${g},${b})`
+    constructor(animation){
+        this.r = Math.floor(Math.random() * 255)
+        this.g = Math.floor(Math.random() * 255)
+        this.b = Math.floor(Math.random() * 255)
+        this.animation = animation
     }
     show(){
         console.log("showing confetti")
@@ -59,11 +64,11 @@ class Confetti{
             position: "absolute",
             left: `${Math.floor(Math.random()*100)}%`,
             top: `${Math.floor(Math.random()*5) - 10}%`,
-            width: this.w,
-            height: this.h,
+            width: `${5 + Math.floor(Math.random() * 10)}px`,
+            height: `${10 + Math.floor(Math.random() * 20)}px`,
             transform: `rotate(${Math.floor(Math.random()*360)}deg)`,
-            backgroundColor: this.color,
-            animationName: "fall",
+            backgroundColor: `rgb(${this.r},${this.g},${this.b})`,
+            animationName: this.animation,
             animationDuration: `${Math.floor(Math.random()*4) + 3}s`,
             animationDelay: `${Math.floor(Math.random()*2)}s`,
             animationTimingFunction: "ease-in"
